aace8798ce7fc8100b9fc1f1659d8dc36acbad18
mariana castro <marianacastrorc@gmail.com>
2025-10-22 17:29:03 -0300

    Restaurando arquivo main

--- src/core/main.ts
+++ src/core/main.ts
@@ -1,6 +1,8 @@
 import Phaser from "phaser";
 import { GAME_CONFIG } from "./config.js";
-import TrafficSignsGameScene from "./TrafficSignsGameScene.js";
+import GameScene from "./GameScene.js";
+// import TrafficSignsGameScene from "./TrafficSignsGameScene.js";
 
-const config = { ...GAME_CONFIG, scene: [TrafficSignsGameScene] };
+// const config = { ...GAME_CONFIG, scene: [TrafficSignsGameScene] };
+const config = { ...GAME_CONFIG, scene: [GameScene] };
 new Phaser.Game(config);
