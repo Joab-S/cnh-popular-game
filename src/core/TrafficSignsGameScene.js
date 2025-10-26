ba94a8d0bb05bb02072212d385660307d732ff08
Joab-S <joabsilva2002@gmail.com>
2025-10-26 17:17:31 -0300

    tt

--- src/core/TrafficSignsGameScene.js
+++ src/core/TrafficSignsGameScene.js
@@ -1,6 +1,6 @@
 export default class TrafficSignsGameScene extends Phaser.Scene {
   constructor() {
-    super('TrafficSignsGameScene');
+    super("TrafficSignsGameScene");
     this.score = 0;
     this.maxScore = 10;
     this.isGameActive = false;
@@ -14,17 +14,17 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
     this.startScreenTexts = [];
     this.spaceKeyCooldown = false;
     this.allSignTypes = [
-      { key: 'sign_go', name: 'Siga Adiante' },
-      { key: 'sign_stop', name: 'Pare' },
-      { key: 'sign_pedestrian', name: 'Pedestre' },
-      { key: 'sign_prohibited', name: 'Proibido' },
-      { key: 'sign_roundabout', name: 'Rotatória' },
-      { key: 'sign_curvy', name: 'Via Sinuosa' }
+      { key: "sign_go", name: "Siga Adiante" },
+      { key: "sign_stop", name: "Pare" },
+      { key: "sign_pedestrian", name: "Pedestre" },
+      { key: "sign_prohibited", name: "Proibido" },
+      { key: "sign_roundabout", name: "Rotatória" },
+      { key: "sign_curvy", name: "Via Sinuosa" },
     ];
   }
 
   preload() {
-    this.load.image('bg_game_1', './assets/images/bg_game_1.png');
+    this.load.image("bg_game_1", "./assets/images/bg_game_1.png");
     this.load.image("sign_go", "./assets/images/placa_siga.png");
     this.load.image("sign_stop", "./assets/images/placa_pare.png");
     this.load.image("sign_pedestrian", "./assets/images/placa_pedestre.png");
@@ -35,19 +35,24 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
 
   create() {
     const { width, height } = this.scale;
-    
-    this.add.image(width / 2, height / 2, 'bg_game_1').setDisplaySize(width, height);
-    
+
+    this.add
+      .image(width / 2, height / 2, "bg_game_1")
+      .setDisplaySize(width, height);
+
     this.setupStartScreen();
-    
-    this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-    this.spaceKey.on('down', this.handleSpacePress, this);
 
-    this.gameOverlay = this.add.rectangle(width / 2, height / 2, width, height, 0x000000)
-    .setAlpha(0)
-    .setDepth(0);
+    this.spaceKey = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
+    this.spaceKey.on("down", this.handleSpacePress, this);
 
-    this.input.on('gameobjectdown', this.handleSignClick, this);
+    this.gameOverlay = this.add
+      .rectangle(width / 2, height / 2, width, height, 0x000000)
+      .setAlpha(0)
+      .setDepth(0);
+
+    this.input.on("gameobjectdown", this.handleSignClick, this);
   }
 
   setupStartScreen() {
@@ -56,38 +61,56 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
     const randomIndex = Phaser.Math.Between(0, this.allSignTypes.length - 1);
     this.targetSignType = this.allSignTypes[randomIndex];
 
-    const titleText = this.add.text(width / 2, height / 2 - 170, 'EXAME TEÓRICO', {
+    const titleText = this.add
+      .text(width / 2, height / 2 - 170, "EXAME TEÓRICO", {
         fontFamily: '"Silkscreen", "Courier New", monospace',
-        fontSize: '28px',
-        color: '#000000',
-        fontWeight: 'bold',
-        align: 'center',
-    }).setOrigin(0.5);
+        fontSize: "28px",
+        color: "#000000",
+        fontWeight: "bold",
+        align: "center",
+      })
+      .setOrigin(0.5);
 
     this.startScreenTexts.push(titleText);
 
-    const targetSignImage = this.add.image(width / 2, height / 2 - 100, this.targetSignType.key)
+    const targetSignImage = this.add
+      .image(width / 2, height / 2 - 100, this.targetSignType.key)
       .setScale(0.08);
     this.startScreenTexts.push(targetSignImage);
 
-    const instructionText = this.add.text(width / 2, height / 2 - 40, `Pressione a BARRA DE ESPAÇO ou clique na placa "${this.targetSignType.name}" quando ela aparecer`, {
-      fontSize: '18px',
-      color: '#000000',
-      fontFamily: '"Silkscreen", "Courier New", monospace',
-      fontWeight: '300',
-      wordWrap: { width: width * 0.8 },
-      align: 'center'
-    }).setOrigin(0.5).setAlpha(0.9);
-    
+    const instructionText = this.add
+      .text(
+        width / 2,
+        height / 2 - 40,
+        `Pressione a BARRA DE ESPAÇO ou clique na placa "${this.targetSignType.name}" quando ela aparecer`,
+        {
+          fontSize: "18px",
+          color: "#000000",
+          fontFamily: '"Silkscreen", "Courier New", monospace',
+          fontWeight: "300",
+          wordWrap: { width: width * 0.8 },
+          align: "center",
+        }
+      )
+      .setOrigin(0.5)
+      .setAlpha(0.9);
+
     this.startScreenTexts.push(instructionText);
 
-    this.startText = this.add.text(width / 2, height / 2 + 200, 'Clique em qualquer lugar para começar', {
-      fontSize: '18px',
-      color: '#000000',
-      fontFamily: '"Silkscreen", "Courier New", monospace',
-      fontWeight: 'bold'
-    }).setOrigin(0.5);
-    
+    this.startText = this.add
+      .text(
+        width / 2,
+        height / 2 + 200,
+        "Clique em qualquer lugar para começar",
+        {
+          fontSize: "18px",
+          color: "#000000",
+          fontFamily: '"Silkscreen", "Courier New", monospace',
+          fontWeight: "bold",
+        }
+      )
+      .setOrigin(0.5);
+
     this.startScreenTexts.push(this.startText);
 
     this.tweens.add({
@@ -95,21 +118,23 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
       alpha: 0.3,
       duration: 800,
       yoyo: true,
-      repeat: -1
+      repeat: -1,
     });
 
-    this.input.once('pointerdown', this.startGame, this);
+    this.input.once("pointerdown", this.startGame, this);
   }
 
   setupGameUI() {
     const { width } = this.scale;
 
-    this.scoreText = this.add.text(width / 2, 30, `${this.score}/${this.maxScore}`, {
-      fontSize: '32px',
-      color: '#ffffff',
-      fontFamily: '"Silkscreen", "Courier New", monospace',
-      fontWeight: '300'
-    }).setOrigin(0.5);
+    this.scoreText = this.add
+      .text(width / 2, 30, `${this.score}/${this.maxScore}`, {
+        fontSize: "32px",
+        color: "#ffffff",
+        fontFamily: '"Silkscreen", "Courier New", monospace',
+        fontWeight: "300",
+      })
+      .setOrigin(0.5);
   }
 
   startGame() {
@@ -120,28 +145,28 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
       targets: this.gameOverlay,
       alpha: 0.6,
       duration: 500,
-      ease: 'Power2'
+      ease: "Power2",
     });
-    
+
     this.removeStartScreen();
-    
+
     this.setupGameUI();
-    
+
     this.startSignSpawning();
   }
 
   removeStartScreen() {
-    this.startScreenTexts.forEach(element => {
-      if (element && typeof element.destroy === 'function') {
+    this.startScreenTexts.forEach((element) => {
+      if (element && typeof element.destroy === "function") {
         element.destroy();
       }
     });
     this.startScreenTexts = [];
-    
-    if (this.startText && typeof this.startText.destroy === 'function') {
+
+    if (this.startText && typeof this.startText.destroy === "function") {
       this.startText.destroy();
     }
-    
+
     this.tweens.killTweensOf(this.startText);
   }
 
@@ -149,20 +174,20 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
     for (let i = 0; i < 3; i++) {
       this.spawnSign();
     }
-    
+
     this.spawnTimer = this.time.addEvent({
       delay: 600,
       callback: this.spawnSign,
       callbackScope: this,
-      loop: true
+      loop: true,
     });
   }
 
   spawnSign() {
     if (!this.isGameActive) return;
-    
+
     const { width, height } = this.scale;
-    
+
     const random = Phaser.Math.Between(1, 100);
     let texture, isTargetSign;
 
@@ -170,7 +195,9 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
       texture = this.targetSignType.key;
       isTargetSign = true;
     } else {
-      const nonTargetSigns = this.allSignTypes.filter(sign => sign.key !== this.targetSignType.key);
+      const nonTargetSigns = this.allSignTypes.filter(
+        (sign) => sign.key !== this.targetSignType.key
+      );
       const randomIndex = Phaser.Math.Between(0, nonTargetSigns.length - 1);
       const randomSign = nonTargetSigns[randomIndex];
       texture = randomSign.key;
@@ -178,33 +205,36 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
     }
 
     if (this.targetSign && isTargetSign) {
-      const nonTargetSigns = this.allSignTypes.filter(sign => sign.key !== this.targetSignType.key);
+      const nonTargetSigns = this.allSignTypes.filter(
+        (sign) => sign.key !== this.targetSignType.key
+      );
       const randomIndex = Phaser.Math.Between(0, nonTargetSigns.length - 1);
       const randomSign = nonTargetSigns[randomIndex];
       texture = randomSign.key;
       isTargetSign = false;
     }
-    
+
     const position = this.findNonOverlappingPosition();
     if (!position) {
-      console.log('Não foi possível encontrar posição livre para nova placa');
+      console.log("Não foi possível encontrar posição livre para nova placa");
       return;
     }
-    
+
     const { x, y } = position;
-    
-    const sign = this.add.image(x, y, texture)
+
+    const sign = this.add
+      .image(x, y, texture)
       .setInteractive({ useHandCursor: true })
-      .setData('isTargetSign', isTargetSign)
-      .setData('type', texture)
-      .setData('alreadyScored', false);
-    
+      .setData("isTargetSign", isTargetSign)
+      .setData("type", texture)
+      .setData("alreadyScored", false);
+
     const baseScale = 0.12;
     const scaleVariation = Phaser.Math.FloatBetween(0.9, 1.1);
     const finalScale = baseScale * scaleVariation;
-    
+
     sign.setScale(0);
-    
+
     sign.radius = Math.max(sign.width, sign.height) * finalScale * 0.6;
 
     this.tweens.add({
@@ -212,9 +242,9 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
       scaleX: finalScale,
       scaleY: finalScale,
       duration: 150,
-      ease: 'Back.out'
+      ease: "Back.out",
     });
-    
+
     this.signs.push(sign);
 
     if (isTargetSign) {
@@ -236,28 +266,34 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
     for (let attempt = 0; attempt < maxAttempts; attempt++) {
       const x = Phaser.Math.Between(100, width - 100);
       const y = Phaser.Math.Between(150, height - 100);
-      
+
       let overlaps = false;
-      
+
       for (const existingSign of this.signs) {
-        const distance = Phaser.Math.Distance.Between(x, y, existingSign.x, existingSign.y);
+        const distance = Phaser.Math.Distance.Between(
+          x,
+          y,
+          existingSign.x,
+          existingSign.y
+        );
         if (distance < minDistance) {
           overlaps = true;
           break;
         }
       }
-      
+
       if (!overlaps) {
         return { x, y };
       }
     }
-    
+
     return null;
   }
 
   handleSpacePress() {
-    if (!this.isGameActive || this.isWaitingToStart || this.spaceKeyCooldown) return;
-    
+    if (!this.isGameActive || this.isWaitingToStart || this.spaceKeyCooldown)
+      return;
+
     if (this.targetSign) {
       this.handleCorrectSpacePress();
     } else {
@@ -272,9 +308,9 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
 
   handleSignClick(pointer, gameObject) {
     if (!this.isGameActive || this.isWaitingToStart) return;
-    
-    const isTargetSign = gameObject.getData('isTargetSign');
-    
+
+    const isTargetSign = gameObject.getData("isTargetSign");
+
     if (isTargetSign) {
       this.scoreForSign(gameObject);
     } else {
@@ -283,20 +319,20 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
   }
 
   scoreForSign(sign) {
-    if (sign.getData('alreadyScored')) {
+    if (sign.getData("alreadyScored")) {
       return;
     }
-    
-    sign.setData('alreadyScored', true);
-    
+
+    sign.setData("alreadyScored", true);
+
     this.spaceKeyCooldown = true;
     this.time.delayedCall(300, () => {
       this.spaceKeyCooldown = false;
     });
-    
+
     this.score++;
     this.scoreText.setText(`${this.score}/${this.maxScore}`);
-    
+
     this.tweens.add({
       targets: sign,
       scaleX: sign.scaleX * 1.3,
@@ -305,11 +341,11 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
       yoyo: true,
       onComplete: () => {
         this.removeSign(sign);
-      }
+      },
     });
-    
-    this.showFeedback(sign.x, sign.y, '+1', 0x4cc9f0);
-    
+
+    this.showFeedback(sign.x, sign.y, "+1", 0x4cc9f0);
+
     if (this.score >= this.maxScore) {
       this.endGame(true);
     }
@@ -317,51 +353,58 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
 
   handleWrongSpacePress() {
     if (this.spaceKeyCooldown) return;
-    
+
     this.spaceKeyCooldown = true;
     this.time.delayedCall(300, () => {
       this.spaceKeyCooldown = false;
     });
-    
+
     this.score = Math.max(0, this.score - 1);
     this.scoreText.setText(`${this.score}/${this.maxScore}`);
 
     this.cameras.main.shake(200, 0.005);
 
-    this.showFeedback(this.scale.width / 2, this.scale.height - 50, '-1', 0xf72585);
+    this.showFeedback(
+      this.scale.width / 2,
+      this.scale.height - 50,
+      "-1",
+      0xf72585
+    );
   }
 
   handleWrongClick(sign) {
     this.score = Math.max(0, this.score - 1);
     this.scoreText.setText(`${this.score}/${this.maxScore}`);
-    
+
     this.tweens.add({
       targets: sign,
       scaleX: sign.scaleX * 0.7,
       scaleY: sign.scaleY * 0.7,
       duration: 250,
-      yoyo: true
+      yoyo: true,
     });
 
     this.cameras.main.shake(200, 0.005);
-    
-    this.showFeedback(sign.x, sign.y, '-1', 0xf72585);
+
+    this.showFeedback(sign.x, sign.y, "-1", 0xf72585);
   }
 
   showFeedback(x, y, text, color) {
-    const feedback = this.add.text(x, y - 40, text, {
-      fontSize: '20px',
-      color: '#ffffff',
-      fontFamily: '"Silkscreen", "Courier New", monospace',
-      fontWeight: 'bold'
-    }).setOrigin(0.5);
-    
+    const feedback = this.add
+      .text(x, y - 40, text, {
+        fontSize: "20px",
+        color: "#ffffff",
+        fontFamily: '"Silkscreen", "Courier New", monospace',
+        fontWeight: "bold",
+      })
+      .setOrigin(0.5);
+
     this.tweens.add({
       targets: feedback,
       y: y - 80,
       alpha: 0,
       duration: 1000,
-      onComplete: () => feedback.destroy()
+      onComplete: () => feedback.destroy(),
     });
   }
 
@@ -371,7 +414,7 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
     if (this.targetSign === sign) {
       this.targetSign = null;
     }
-    
+
     this.tweens.add({
       targets: sign,
       scaleX: 0,
@@ -384,42 +427,51 @@ export default class TrafficSignsGameScene extends Phaser.Scene {
           this.signs.splice(index, 1);
         }
         sign.destroy();
-      }
+      },
     });
   }
 
   endGame(isVictory) {
     this.isGameActive = false;
-    
+
     if (this.spawnTimer) this.spawnTimer.remove();
-    
-    this.signs.forEach(sign => sign.destroy());
+
+    this.signs.forEach((sign) => sign.destroy());
     this.signs = [];
     this.targetSign = null;
-    
+
     const { width, height } = this.scale;
-    
-    const overlay = this.add.rectangle(width / 2, height / 2, width, height, 0x000000)
+
+    const overlay = this.add
+      .rectangle(width / 2, height / 2, width, height, 0x000000)
       .setAlpha(0.8);
-    
-    const resultDisplay = this.add.text(width / 2, height / 2 - 30, 'PARABÉNS!', {
-      fontSize: '48px',
-      color: '#ffffff',
-      fontFamily: '"Silkscreen", "Courier New", monospace',
-      fontWeight: 'bold'
-    }).setOrigin(0.5);
-    
-    const scoreDisplay = this.add.text(width / 2, height / 2 + 30, 
-      `Pontuação: ${this.score}/${this.maxScore}`, {
-      fontSize: '24px',
-      color: '#ffffff',
-      fontFamily: '"Silkscreen", "Courier New", monospace',
-      fontWeight: 'bold'
-    }).setOrigin(0.5);
+
+    const resultDisplay = this.add
+      .text(width / 2, height / 2 - 30, "PARABÉNS!", {
+        fontSize: "48px",
+        color: "#ffffff",
+        fontFamily: '"Silkscreen", "Courier New", monospace',
+        fontWeight: "bold",
+      })
+      .setOrigin(0.5);
+
+    const scoreDisplay = this.add
+      .text(
+        width / 2,
+        height / 2 + 30,
+        `Pontuação: ${this.score}/${this.maxScore}`,
+        {
+          fontSize: "24px",
+          color: "#ffffff",
+          fontFamily: '"Silkscreen", "Courier New", monospace',
+          fontWeight: "bold",
+        }
+      )
+      .setOrigin(0.5);
 
     this.time.delayedCall(800, () => {
-      this.events.emit('gameEnded', { victory: isVictory, score: this.score });
-    })
+      this.events.emit("gameEnded", { victory: isVictory, score: this.score });
+    });
   }
 
   update() {
