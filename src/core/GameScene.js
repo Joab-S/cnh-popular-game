5efe96693106de85af796cc6de3f3d1241af2a05
mariana castro <marianacastrorc@gmail.com>
2025-10-26 10:22:15 -0300

    Ajustando imagens

--- src/core/GameScene.js
+++ src/core/GameScene.js
@@ -102,8 +102,8 @@ export default class GameScene extends Phaser.Scene {
     this.load.image("button_action", "./assets/images/button-action.png");
 
     this.load.image("detran", "./assets/images/detran.png");
-    this.load.image("detran_practical_bg", "./assets/images/detran_theoretical_bg.png");
-    this.load.image("detran_practical_bg_2", "./assets/images/detran_theoretical_bg_2.png");
+    this.load.image("detran_theoretical_bg", "./assets/images/detran_theoretical_bg.png");
+    this.load.image("detran_theoretical_bg_2", "./assets/images/detran_theoretical_bg_2.png");
 
     this.load.image("detran_practical_bg", "./assets/images/detran_practical_bg.png");
     this.load.image("detran_practical_bg_2", "./assets/images/detran_practical_bg_2.png");
