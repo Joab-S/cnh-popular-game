3f9331ebecdcd395027432e38e9138dd3eeaddfd
viniciuscosta <viniciussec@hotmail.com>
2025-10-29 13:34:59 -0300

    refactor: Corrigindo cenas

--- src/core/main.ts
+++ src/core/main.ts
@@ -3,10 +3,9 @@ import { GAME_CONFIG } from "./config.js";
 import GameScene from "./GameScene.js";
 import CarGameScene from "./CarGameScene.js";
 import EndCarGameScene from "../engine/ui/EndCarGameScene.js";
-import StartCarGameScene from "../engine/ui/StartCarGameScene.js";
 
 const config = {
   ...GAME_CONFIG,
-  scene: [StartCarGameScene, GameScene, CarGameScene, EndCarGameScene],
+  scene: [GameScene, CarGameScene, EndCarGameScene],
 };
 new Phaser.Game(config);
