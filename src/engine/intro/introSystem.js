05f72e66d25a1e82422211c796db7288e70c5032
Joab-S <joabdev11@gmail.com>
2025-11-01 11:14:22 -0300

    refactor

--- src/engine/intro/introSystem.js
+++ src/engine/intro/introSystem.js
@@ -9,13 +9,13 @@ export class IntroSystem {
     this.canAdvance = true;
     this.eKeyJustPressed = false;
     this.introCompleted = false;
-    
+
     this.instructions = [
       "Bem-vindo ao CNH Popular - O Jogo!",
       "Neste jogo, você vai vivenciar a jornada para obter sua Carteira Nacional de Habilitação através do programa CNH Popular do Ceará.",
       "Você precisará passar por todas as etapas: documentos, aulas teóricas, exames médicos, aulas práticas e as provas finais.",
       "CONTROLS_SCREEN",
-      "Explore os ambientes, complete missões e boa sorte na sua jornada!"
+      "Explore os ambientes, complete missões e boa sorte na sua jornada!",
     ];
 
     this.coverImage = null;
@@ -37,41 +37,57 @@ export class IntroSystem {
   }
 
   shouldShowIntro() {
-    return (this.showingCover || this.showingInstructions) && !this.introCompleted;
+    return (
+      (this.showingCover || this.showingInstructions) && !this.introCompleted
+    );
   }
 
   showCoverScreen() {
     const { width, height } = this.scene.scale;
 
-    this.coverImage = this.scene.add.image(width / 2, height / 2, "capa")
+    this.coverImage = this.scene.add
+      .image(width / 2, height / 2, "capa")
       .setDisplaySize(width, height)
       .setInteractive();
 
-    this.scene.textures.get("capa").setFilter(Phaser.Textures.FilterMode.NEAREST);
-    this.scene.textures.get("logo").setFilter(Phaser.Textures.FilterMode.LINEAR);
+    this.scene.textures
+      .get("capa")
+      .setFilter(Phaser.Textures.FilterMode.NEAREST);
+    this.scene.textures
+      .get("logo")
+      .setFilter(Phaser.Textures.FilterMode.LINEAR);
 
-    this.overlayImage = this.scene.add.image(width / 2, height / 3 - 120, "logo")
+    this.overlayImage = this.scene.add
+      .image(width / 2, height / 3 - 120, "logo")
       .setScale(0.5)
       .setDepth(1);
 
-    this.titleText = this.scene.add.text(width / 2, height / 3 + 170, 
-      "CNH POPULAR - O JOGO", 
-      {
+    this.titleText = this.scene.add
+      .text(width / 2, height / 3 + 170, "CNH POPULAR - O JOGO", {
         fontFamily: '"Silkscreen", monospace',
         fontSize: "48px",
         color: "#ffffff",
         fontStyle: "bold",
-      }
-    ).setOrigin(0.5).setDepth(2);
+      })
+      .setOrigin(0.5)
+      .setDepth(2);
 
-    this.progressText = pressKeyIcon(this.scene, "Pressione", "ou clique na tela para continuar", width / 2, height - 80, "button_action", 0.15);
+    this.progressText = pressKeyIcon(
+      this.scene,
+      "Pressione",
+      "ou clique na tela para continuar",
+      width / 2,
+      height - 80,
+      "button_action",
+      0.15
+    );
 
     this.scene.tweens.add({
       targets: this.progressText,
       alpha: 0.2,
       duration: 800,
       yoyo: true,
-      repeat: -1
+      repeat: -1,
     });
 
     const advance = () => {
@@ -88,28 +104,40 @@ export class IntroSystem {
   showInstructionsScreen() {
     const { width, height } = this.scene.scale;
 
-    this.instructionsBg = this.scene.add.image(width / 2, height / 2, "capa")
+    this.instructionsBg = this.scene.add
+      .image(width / 2, height / 2, "capa")
       .setDisplaySize(width, height)
       .setInteractive();
 
-    this.scene.textures.get("capa").setFilter(Phaser.Textures.FilterMode.NEAREST);
+    this.scene.textures
+      .get("capa")
+      .setFilter(Phaser.Textures.FilterMode.NEAREST);
 
-    this.dialogBox = this.scene.add.rectangle(width / 2, height - 230, width - 100, 200, 0xffffff, 0.9)
+    this.dialogBox = this.scene.add
+      .rectangle(width / 2, height - 230, width - 100, 200, 0xffffff, 0.9)
       .setStrokeStyle(2, 0x000000)
       .setDepth(1);
 
     this.renderCurrentInstruction(width, height);
-    this.progressText = pressKeyIcon(this.scene, "Pressione", "ou clique na tela para continuar", width / 2, height - 80, "button_action", 0.15);
+    this.progressText = pressKeyIcon(
+      this.scene,
+      "Pressione",
+      "ou clique na tela para continuar",
+      width / 2,
+      height - 80,
+      "button_action",
+      0.15
+    );
 
     this.scene.tweens.add({
       targets: this.progressText,
       alpha: 0.2,
       duration: 800,
       yoyo: true,
-      repeat: -1
+      repeat: -1,
     });
 
-    this.createBackButton()
+    this.createBackButton();
 
     const next = () => {
       if (this.canAdvance) {
@@ -127,7 +155,7 @@ export class IntroSystem {
       this.instructionDialog = null;
     }
     if (this.visualControls) {
-      this.visualControls.forEach(el => el.destroy());
+      this.visualControls.forEach((el) => el.destroy());
       this.visualControls = null;
     }
 
@@ -136,29 +164,28 @@ export class IntroSystem {
     if (current === "CONTROLS_SCREEN") {
       this.renderControlsScreen(width, height);
     } else {
-      this.instructionDialog = this.scene.add.text(width / 2, height - 230, 
-        current, 
-        {
+      this.instructionDialog = this.scene.add
+        .text(width / 2, height - 230, current, {
           fontFamily: '"Silkscreen", monospace',
           fontSize: "18px",
           color: "#000000",
           wordWrap: { width: width - 150 },
           align: "center",
-          lineSpacing: 10
-        }
-      ).setOrigin(0.5).setDepth(2);
+          lineSpacing: 10,
+        })
+        .setOrigin(0.5)
+        .setDepth(2);
     }
   }
 
   nextInstruction() {
-    
     if (this.visualControls) {
-      this.visualControls.forEach(el => el.destroy());
+      this.visualControls.forEach((el) => el.destroy());
       this.visualControls = null;
     }
-    
+
     this.currentInstruction++;
-    
+
     this.createBackButton();
 
     if (this.currentInstruction < this.instructions.length) {
@@ -166,15 +193,23 @@ export class IntroSystem {
       this.renderCurrentInstruction(width, height);
 
       if (this.progressText) this.progressText.destroy();
-      this.progressText = pressKeyIcon(this.scene, "Pressione", "ou clique na tela para continuar", width / 2, height - 80, "button_action", 0.15);
-      
+      this.progressText = pressKeyIcon(
+        this.scene,
+        "Pressione",
+        "ou clique na tela para continuar",
+        width / 2,
+        height - 80,
+        "button_action",
+        0.15
+      );
+
       this.scene.tweens.add({
         targets: this.progressText,
         alpha: 0.2,
         duration: 800,
         yoyo: true,
         repeat: -1,
-        ease: "Sine.easeInOut"
+        ease: "Sine.easeInOut",
       });
 
       this.scene.time.delayedCall(300, () => {
@@ -190,7 +225,7 @@ export class IntroSystem {
     this.showingInstructions = true;
     this.currentInstruction = 0;
     this.canAdvance = true;
-    
+
     this.cleanupCoverScreen();
     this.scene.time.delayedCall(100, () => {
       this.showInstructionsScreen();
@@ -201,7 +236,7 @@ export class IntroSystem {
     this.showingInstructions = false;
     this.introCompleted = true;
     this.cleanupInstructionsScreen();
-    
+
     this.scene.time.delayedCall(100, () => {
       if (this.scene.setupCharacterSelection) {
         this.scene.setupCharacterSelection((character) => {
@@ -225,7 +260,7 @@ export class IntroSystem {
     if (this.instructionDialog) this.instructionDialog.destroy();
     if (this.progressText) this.progressText.destroy();
     if (this.visualControls) {
-      this.visualControls.forEach(el => el.destroy());
+      this.visualControls.forEach((el) => el.destroy());
       this.visualControls = null;
     }
     if (this.backButton) {
@@ -244,11 +279,21 @@ export class IntroSystem {
       this.renderCurrentInstruction(width, height);
 
       if (this.instructionDialog) {
-        this.instructionDialog.setText(this.instructions[this.currentInstruction]);
+        this.instructionDialog.setText(
+          this.instructions[this.currentInstruction]
+        );
       }
 
       if (this.progressText) this.progressText.destroy();
-      this.progressText = pressKeyIcon(this.scene, "Pressione", "ou clique na tela para continuar", width / 2, height - 80, "button_action", 0.15);
+      this.progressText = pressKeyIcon(
+        this.scene,
+        "Pressione",
+        "ou clique na tela para continuar",
+        width / 2,
+        height - 80,
+        "button_action",
+        0.15
+      );
 
       this.scene.tweens.add({
         targets: this.progressText,
@@ -278,15 +323,25 @@ export class IntroSystem {
     if (!this.scene.keys || !this.scene.keys.E) return this.shouldShowIntro();
 
     const eKeyPressed = this.scene.keys.E.isDown;
-    
-    if (this.showingCover && eKeyPressed && this.canAdvance && !this.eKeyJustPressed) {
+
+    if (
+      this.showingCover &&
+      eKeyPressed &&
+      this.canAdvance &&
+      !this.eKeyJustPressed
+    ) {
       this.eKeyJustPressed = true;
       this.canAdvance = false;
       this.advanceFromCover();
       return true;
     }
 
-    if (this.showingInstructions && eKeyPressed && this.canAdvance && !this.eKeyJustPressed) {
+    if (
+      this.showingInstructions &&
+      eKeyPressed &&
+      this.canAdvance &&
+      !this.eKeyJustPressed
+    ) {
       this.eKeyJustPressed = true;
       this.canAdvance = false;
       this.nextInstruction();
@@ -302,46 +357,61 @@ export class IntroSystem {
     return {
       showingCover: this.showingCover,
       showingInstructions: this.showingInstructions,
-      introCompleted: this.introCompleted
+      introCompleted: this.introCompleted,
     };
   }
 
   renderControlsScreen(width, height) {
-    const title = this.scene.add.text(width / 2, this.dialogBox.height - 50, "USE AS TECLAS", {
-      fontFamily: '"Silkscreen", monospace',
-      fontSize: "22px",
-      color: "#000000",
-    }).setOrigin(0.5).setDepth(2);
+    const title = this.scene.add
+      .text(width / 2, this.dialogBox.height - 50, "USE AS TECLAS", {
+        fontFamily: '"Silkscreen", monospace',
+        fontSize: "22px",
+        color: "#000000",
+      })
+      .setOrigin(0.5)
+      .setDepth(2);
 
-    const wasd = this.scene.add.image(width / 2 - 220, height - 230, "wasd_keys")
-      .setScale(0.35).setDepth(2);
-    const arrows = this.scene.add.image(width / 2 - 80, height - 230, "arrow_keys")
-      .setScale(0.35).setDepth(2);
+    const wasd = this.scene.add
+      .image(width / 2 - 220, height - 230, "wasd_keys")
+      .setScale(0.35)
+      .setDepth(2);
+    const arrows = this.scene.add
+      .image(width / 2 - 80, height - 230, "arrow_keys")
+      .setScale(0.35)
+      .setDepth(2);
 
-    const moveText = this.scene.add.text(width / 2 - 150, height - 160, "para se movimentar", {
-      fontFamily: '"Silkscreen", monospace',
-      fontSize: "16px",
-      color: "#000000",
-    }).setOrigin(0.5).setDepth(2);
+    const moveText = this.scene.add
+      .text(width / 2 - 150, height - 160, "para se movimentar", {
+        fontFamily: '"Silkscreen", monospace',
+        fontSize: "16px",
+        color: "#000000",
+      })
+      .setOrigin(0.5)
+      .setDepth(2);
 
-    const keyE = this.scene.add.image(width / 2 + 180, height - 230, "button_action")
-      .setScale(0.2).setDepth(2);
+    const keyE = this.scene.add
+      .image(width / 2 + 180, height - 230, "button_action")
+      .setScale(0.2)
+      .setDepth(2);
 
-    const interactText = this.scene.add.text(width / 2 + 180, height - 160, "para interagir", {
-      fontFamily: '"Silkscreen", monospace',
-      fontSize: "16px",
-      color: "#000000",
-    }).setOrigin(0.5).setDepth(2);
+    const interactText = this.scene.add
+      .text(width / 2 + 180, height - 160, "para interagir", {
+        fontFamily: '"Silkscreen", monospace',
+        fontSize: "16px",
+        color: "#000000",
+      })
+      .setOrigin(0.5)
+      .setDepth(2);
 
     this.visualControls = [title, wasd, arrows, moveText, keyE, interactText];
   }
 
   createBackButton() {
     const { width, height } = this.scene.scale;
-    
+
     if (this.currentInstruction > 0 && !this.backButton) {
       this.backButton = this.scene.add
-        .image(width / 2 - (width / 2.5), height - 153, "arrow_left")
+        .image(width / 2 - width / 2.5, height - 153, "arrow_left")
         .setOrigin(0.5)
         .setDepth(3)
         .setScale(0.05)
@@ -349,10 +419,18 @@ export class IntroSystem {
 
       this.backButton.on("pointerover", () => {
         if (this.currentInstruction > 0)
-          this.scene.tweens.add({ targets: this.backButton, scale: 0.045, duration: 150 });
+          this.scene.tweens.add({
+            targets: this.backButton,
+            scale: 0.045,
+            duration: 150,
+          });
       });
       this.backButton.on("pointerout", () => {
-        this.scene.tweens.add({ targets: this.backButton, scale: 0.05, duration: 150 });
+        this.scene.tweens.add({
+          targets: this.backButton,
+          scale: 0.05,
+          duration: 150,
+        });
       });
       this.backButton.on("pointerdown", () => {
         if (this.canAdvance) {
